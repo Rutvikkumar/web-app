ec746592b585026da1fd9c20797698113040ca4e
Rutvikkumar <parmarrutvik1414@gmail.com>
2024-07-17 10:08:13 +0530

    comple

--- Frontend/src/componets/Navbar.jsx
+++ Frontend/src/componets/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar() {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Homepage</a>
+                <a href="/">Homepage</a>
               </li>
               <li>
                 <a href="/login">Portfolio</a>
